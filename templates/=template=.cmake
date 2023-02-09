aabe5297e0f205904d04cb38edc62dd41ad057be
Stephan Seitz <sseitz@nvidia.com>
2023-02-09 21:19:35 +0100

    Update cmake template

--- templates/=template=.cmake
+++ templates/=template=.cmake
@@ -2,28 +2,16 @@ project(%HERE%%FDIR%)
 
 cmake_minimum_required(VERSION 3.20)
 
-if(MSVC)
-  # Force to always compile with W4
-  if(CMAKE_CXX_FLAGS MATCHES "/W[0-4]")
-    string(REGEX REPLACE "/W[0-4]" "/W4" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
-  else()
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4")
-  endif()
-elseif(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
-  target_add_compile_options("-Wall -Wno-long-long -pedantic")
+if (MSVC)
+    # warning level 4
+    add_compile_options(/W4)
+else()
+    # additional warnings
+    add_compile_options(-Wall -Wextra -Wpedantic)
 endif()
-
-aux_source_directory(src SOURCES)
 
 add_executable(%FDIR%
     main.cpp
-    ${SOURCES}
-    )
-
-target_include_directories(
-    %FDIR%
-    PRIVATE
-    ${CMAKE_CURRENT_SOURCE_DIR}/include/
     )
 
 set_target_properties(%FDIR% PROPERTIES
